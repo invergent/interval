1c02ebc49eaff445ccad6e175ae4601a99c58c76
Invergent Technologies <invergenttechnologies@gmail.com>
2019-09-03 23:35:01 +0100

    create sign up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { HomePage } from './home/home.page';
+import { SignupPage } from './signup/signup.page';
 
 const routes: Routes = [
   { path: '', component: HomePage },
+  { path: 'signup', component: SignupPage },
   {
     path: 'timer',
     loadChildren: () => import('./timer/timer.module').then(m => m.TimerModule)
-  }
+  },
+  
 ];
 
 @NgModule({
---
+++ src/app/signup/signup.page.spec.ts
@@ -0,0 +1,13 @@
+import { SignupPage } from './signup.page';
+
+describe('SignupPage', () => {
+  let component: SignupPage;
+
+  beforeEach(() => {
+    component = new SignupPage();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
